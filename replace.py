bc3fccfd619e64b8b764961a0d431014abccfbc7
EdmundFu-233 <120171892+EdmundFu-233@users.noreply.github.com>
2023-03-05 21:32:20 +0800

    upload the file

---
+++ replace.py
@@ -0,0 +1,7 @@
+import os
+import time
+while 1 == 1:
+    os.system("cp Chat.txt Chat_nmsc.txt")
+    os.system("cp Chat.txt Chat_tld.txt")
+    os.system("cp Chat.txt Chat_gz.txt")
+    time.sleep(300)
---
+++ replace_onetime.py
@@ -0,0 +1,4 @@
+import os
+os.system("cp Chat.txt Chat_nmsc.txt")
+os.system("cp Chat.txt Chat_tld.txt")
+os.system("cp Chat.txt Chat_gz.txt")
